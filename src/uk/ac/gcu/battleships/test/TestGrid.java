dfede178dd899d6357d0a2e1ab991390f84e79fe
Yann Prono <pronoyann@gmail.com>
2014-05-07 00:08:24 +0200

    Implements all test methods for the Grid class.

--- src/uk/ac/gcu/battleships/test/TestGrid.java
+++ src/uk/ac/gcu/battleships/test/TestGrid.java
@@ -1,8 +1,11 @@
 package uk.ac.gcu.battleships.test;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 import uk.ac.gcu.battleships.Grid;
+import uk.ac.gcu.battleships.Ship;
 
 /** 
  * Class for testing the Grid.
@@ -85,7 +88,92 @@ public class TestGrid {
 		// TEST
 		char res = g.returnCharacter(10, row);
 
-		assertEquals("The result should be ' ' (Exception)",' ',res);
+		assertEquals("The result should be ' ' (Exception)",0,res);
+	}
+	
+	/** 
+	 * Test the addShip method (horizontal Ship).
+	 */
+	@Test
+	public void testAddShipHorizon() {		
+		// Initialisation
+		Grid g = new Grid(5);
+		boolean ok = false;
+		Ship s = new Ship(4,0,0,'h');
+		Ship s2 = new Ship(3,2,2,'h');
+		
+		
+		ok = g.addShip(s);
+		boolean ok2 = g.addShip(s2);
+		int nb = g.getShips().size();
+
+		// TEST
+		assertTrue("The result should be TRUE",ok);
+		assertTrue("The result should be TRUE",ok2);
+		assertEquals("They should have 2 ships on the grid",2,nb);
+	}
+	
+	/** 
+	 * Test the addShip method (vertical Ship).
+	 */
+	@Test
+	public void testAddShipVertical() {		
+		// Initialisation
+		Grid g = new Grid(5);
+		boolean ok = false;
+		Ship s = new Ship(3,0,0,'v');
+		Ship s2 = new Ship(2,1,2,'v');
+	
+		ok = g.addShip(s);
+		boolean ok2 = g.addShip(s2);
+		System.out.println(g);
+		int nb = g.getShips().size();
+
+		// TEST
+		
+		assertTrue("The result 1 should be TRUE",ok);
+		assertTrue("The result should be TRUE",ok2);
+		assertEquals("They should have 2 ships on the grid",2,nb);
+	}
+	
+	/** 
+	 * Test the addShip method when a Ship is too bigger relative to the Grid.
+	 */
+	@Test
+	public void testAddShip_OutOfBounds() {		
+		// Initialisation
+		Grid g = new Grid(3);
+		boolean ok = false;
+		Ship s = new Ship(4,0,0,'h');
+
+		ok = g.addShip(s);
+		int nb = g.getShips().size();
+
+		// TEST
+		
+		assertTrue("The result should be FALSE",!ok);
+		assertEquals("They should no have ships on the grid",0,nb);
+	}
+	
+	/** 
+	 * Test the addShip method with conflicts between two ships.
+	 */
+	@Test
+	public void testAddShip_ConflictShips() {		
+		// Initialisation
+		Grid g = new Grid(3);
+		boolean ok = false;
+		Ship s = new Ship(2,0,0,'h');
+		Ship s2 = new Ship(2,0,1,'v');
+
+		ok = g.addShip(s);
+		boolean ok2 = g.addShip(s2);
+		int nb = g.getShips().size();
+
+		// TEST
+		assertTrue("The result should be TRUE",ok);
+		assertFalse("The result should be FALSE",ok2);
+		assertEquals("They should have onyl a ship on the grid",1,nb);
 	}
 
 }
