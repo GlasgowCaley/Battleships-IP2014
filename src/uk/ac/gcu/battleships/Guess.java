6912dec85d7927b3e5525d5ddbccddcc043b2a58
Yann Prono <pronoyann@gmail.com>
2014-05-10 01:03:53 +0200

    Changes bytes into Integers in Guess class.

--- src/uk/ac/gcu/battleships/Guess.java
+++ src/uk/ac/gcu/battleships/Guess.java
@@ -1,20 +1,20 @@
 package uk.ac.gcu.battleships;
 
 public class Guess {
-	private byte x_coord, y_coord;
+	private int x_coord, y_coord;
 	
 	//CONSTRUCTORS
 	
-	public Guess(byte x, byte y){
+	public Guess(int x, int y){
 		x_coord=x;
 		y_coord=y;
 	}
 	//METHODS
-	public byte get_X(){
+	public int get_X(){
 		return x_coord;
 	}
 	
-	public byte get_Y(){
+	public int get_Y(){
 		return y_coord;
 	}
 }
