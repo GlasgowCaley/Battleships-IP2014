cab891ee9f643eae5a568204af38eb2cf079c881
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-10 20:53:14 +0100

    Minor change to Player class
    Changed myGrid = new Grid to myGrid = new Grid(10). Player and Game
    classes should now communicate properly. Also added testing code which
    is commented for now.

--- src/uk/ac/gcu/battleships/Player.java
+++ src/uk/ac/gcu/battleships/Player.java
@@ -1,28 +1,33 @@
 package uk.ac.gcu.battleships;
 
 public class Player  {
-	public String name;
-	public Grid myGrid;
-	
-	public Player(){
-		myGrid = new Grid;
-	}
-	
-	public void createShip(int x, int y, int size, char orientation){
-		Ship S = new Ship(size, x, y, orientation);
-		myGrid.addShip(S);
-	}
-	
-	public boolean makeGuess(Guess G){
-		return(G.get_X()==2&&G.get_Y()==2);
-	}
-	
-	//TESTING
+    public String name;
+    public Grid myGrid;
+     
+    public Player(){
+        myGrid = new Grid(10);
+    }
+     
+ 
+    public void createShip(int size, int x, int y, char orientation){
+        Ship S = new Ship(size, x, y, orientation);
+        myGrid.addShip(S);
+    }
+ 
+     
+    public boolean makeGuess(Guess G){
+        return(G.get_X()==2&&G.get_Y()==2);
+    }
+     
+    //TESTING
+    /*
 public static void main(String[] args) {
-		Player p1 = new Player();	
-		Guess g1 = new Guess((byte)2,(byte)2);					
-		Guess g2 = new Guess((byte)2,(byte)1);					
-		System.out.println(p1.makeGuess(g1));
-		System.out.println(p1.makeGuess(g2));					
-	}
+        Game game = new Game();
+        Player p1 = new Player();   
+        Guess g1 = new Guess((byte)2,(byte)2);                  
+        Guess g2 = new Guess((byte)2,(byte)1);                  
+        System.out.println(p1.makeGuess(g1));
+        System.out.println(p1.makeGuess(g2));                   
+    }
+    */
 }
