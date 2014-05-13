f7f40f8e2a31e5c1c48fdf69286507f42d2aef9f
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-13 21:16:59 +0100

    Created GameRunner class for main method
    Also minor changes to Game

---
+++ src/uk/ac/gcu/battleships/GameRunner.java
@@ -0,0 +1,12 @@
+package uk.ac.gcu.battleships;
+
+/* This is the main class containing only the main method
+ * Instantiates new Game object
+ */
+public class GameRunner {
+	 public static void main(String[] args) {
+	    	Game game = new Game();
+	    }
+	    
+}
+
