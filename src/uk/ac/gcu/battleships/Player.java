315490ca9e860a94cac6f8d8353e6c0594395b32
pablosjv <pablosjv@hotmail.com>
2014-05-14 11:03:01 +0100

    Add the methods addFleet and addBoat. Also there is the method changeX which will change any letter into a character.

--- src/uk/ac/gcu/battleships/Player.java
+++ src/uk/ac/gcu/battleships/Player.java
@@ -1,5 +1,7 @@
 package uk.ac.gcu.battleships;
 
+import java.util.Scanner;
+
 public class Player  {
     public String name;
     public Grid myGrid;
@@ -8,7 +10,16 @@ public class Player  {
     public Player(){
         myGrid = new Grid();
     }
-     
+    
+    public Player(String n){
+    	name = n;
+    }
+
+    public Player(String n, int size){
+    	name = n;
+    	myGrid = new Grid(size);
+    }
+
  
     public boolean createShip(int size, int x, int y, char orientation){
         Ship S = new Ship(size, x, y, orientation);
@@ -17,9 +28,64 @@ public class Player  {
  
      
     public boolean makeGuess(Guess G){
-        return(G.get_X()==2&&G.get_Y()==2);
+        return opponentGrid.checkGuess(G);
     }
-     
+    
+    public void addFleet(){
+    	
+    	for(int i=2; i<=4; i++){
+    		if (i==2) System.out.println("DESTROYER: " );
+    			else if (i==3)  System.out.println("SUBMARINE: " );
+    				else System.out.println("BATTLESHIP: " );
+    		this.addBoat(i);
+    	}
+    	
+    	
+    }
+    public void addBoat(int b){
+    	 Scanner input = new Scanner(System.in);
+         String temp = input.nextLine();
+        
+ 	
+     	//get position of ship
+        System.out.println("Please enter the horrizontal(A to E) coordinate of ship " ); //+ count
+     	input = new Scanner(System.in);
+     	while (!input.hasNextInt()) {
+     		System.out.println("invalid! You must enter an letter");
+     		input.next();
+     	}
+         temp = input.nextLine();
+         char horiz =  (char) Integer.parseInt(temp); //The player inputs a character for the x coordinates 
+         
+     	System.out.println("Please enter the vertical(1 to 5) coordinate of ship " ); //+ count
+     	input = new Scanner(System.in);
+     	while (!input.hasNextInt()) {
+     		System.out.println("invalid! You must enter an integer");
+     		input.next();
+     	}
+         temp = input.nextLine();
+         int vert =  Integer.parseInt(temp);
+         
+     	//get orientation of ship -- presumably v h
+         System.out.println("Please enter the Orientation(v or h) coordinate of ship " ); //+ count
+     	input = new Scanner(System.in);
+         temp = input.nextLine();
+         char orient = temp.charAt(0);
+         
+     	//call player.createShip method
+         this.createShip(b,changeX(horiz),vert,orient);
+         
+    }
+    
+    public int changeX(char c){ //The method changes any letter into a number
+    	int i=0;
+    	boolean enc=false;
+    	while(i<=27 && !enc){
+    		if(c-'a'==i||c-'A'==i) enc=true;
+    		i++;
+    	}
+    	return i;
+    }
     //TESTING
     /*
 public static void main(String[] args) {
