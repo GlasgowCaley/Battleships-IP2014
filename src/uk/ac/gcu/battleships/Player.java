b598b5052f1cfa3146a90b94a65ee2113063d01e
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-14 15:52:56 +0100

    Broken Player class for Tuomas

--- src/uk/ac/gcu/battleships/Player.java
+++ src/uk/ac/gcu/battleships/Player.java
@@ -1,6 +1,7 @@
 package uk.ac.gcu.battleships;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class Player  {
     public String name;
@@ -45,18 +46,27 @@ public class Player  {
     public void addBoat(int b){
     	 Scanner input = new Scanner(System.in);
          String temp = input.nextLine();
-        
- 	
+         
+         temp.length();
+         
+         
+
      	//get position of ship
+         char c = 0 ;
         System.out.println("Please enter the horizontal(A to E) coordinate of ship " ); //+ count
      	input = new Scanner(System.in);
-     	while (!input.hasNextInt()) {
-     		System.out.println("invalid! You must enter a letter");
+     	
+     	do {
+     		System.out.println("You must enter a single character");
+     		temp=input.next(); 
+     		}while (temp.length() != 1);
+     		
+     	
+     	if ((c < 'a'|| c >'e') && (c <= 'A'|| c >'E' )) {
+     		System.out.println("this character is not within the correct range");
      		input.next();
      	}
-         temp = input.nextLine();
-         char horiz =  (char) Integer.parseInt(temp); //The player inputs a character for the x coordinates 
-         
+     		
      	System.out.println("Please enter the vertical(1 to 5) coordinate of ship " ); //+ count
      	input = new Scanner(System.in);
      	while (!input.hasNextInt()) {
@@ -67,13 +77,13 @@ public class Player  {
          int vert =  Integer.parseInt(temp);
          
      	//get orientation of ship -- presumably v h
-         System.out.println("Please enter the Orientation(v or h) coordinate of ship " ); //+ count
+        System.out.println("Please enter the Orientation(v or h) coordinate of ship " ); //+ count
      	input = new Scanner(System.in);
-         temp = input.nextLine();
-         char orient = temp.charAt(0);
+        temp = input.nextLine();
+        char orient = temp.charAt(0);
          
      	//call player.createShip method
-         this.createShip(b,changeX(horiz),vert,orient);
+        this.createShip(b,(this.changeX(c)),vert,orient);
          
          
     }
