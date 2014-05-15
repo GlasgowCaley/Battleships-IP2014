c56601e0f9a528385cb796344ce2c83a6393f6c1
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-15 13:53:00 +0100

    Unfinished TestGame class

---
+++ src/uk/ac/gcu/battleships/test/TestGame.java
@@ -0,0 +1,14 @@
+package uk.ac.gcu.battleships.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGame {
+
+	@Test
+	public void testFailure() throws Exception {
+		
+	}
+
+}
