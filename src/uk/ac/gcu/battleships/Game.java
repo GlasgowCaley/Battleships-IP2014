854afe383895a67846d658152d6b6c9401063aa7
Yann Prono <pronoyann@gmail.com>
2014-05-15 18:30:45 +0200

    Add results at the end of a game.

--- src/uk/ac/gcu/battleships/Game.java
+++ src/uk/ac/gcu/battleships/Game.java
@@ -19,7 +19,7 @@ public class Game
 
 	// Instantiate Guess
 	private Guess g;
-	
+
 	// Game method, calls showMenu and kick-starts the game
 	public Game() 
 	{
@@ -78,7 +78,7 @@ public class Game
 	private void newGame()
 	{
 		// AI or Player?
-		
+
 		// Confirmation message
 		System.out.println("New game created!");
 
@@ -110,126 +110,97 @@ public class Game
 		int x; // x coordinate
 		int y; // y coordinate
 
-
 		do {
-			Game.clearConsole(); // Empty the screen		
-			System.out.println("Captain " +player[turn].name + " and his pirate crew"); // Display name of player whose turn it is
-			sc.nextLine();
-			System.out.println("Your Fleet");
-			System.out.println(display(player[turn].myGrid.displayOwnGrid())); // Show own grid
-			System.out.println("Your guesses");
-			System.out.println(display(player[turn].opponentGrid.displayEnnemyGrid())); // Show enemy grid
-			System.out.println("Enter your guess:  "); // Ask for guess
-			boolean same = false;
-			while(!same) {
-				// 	Read the guess
-				System.out.println("Enter X coordinate :");
-				x = Game.readXAxis(); // Call readXAxis
-				this.g.set_X(x);
+			win = player[turn].enterGuess(g);
 		
-				System.out.println("Enter Y coordinate :");
-				y = Game.readYAxis(); // Call readYAxis
-				this.g.set_Y(y);
-				char c = player[turn].opponentGrid.returnCharacter(y, x);
-			
-				if(c == Grid.HIT_MISSED || c == Grid.HIT) {
-					same = false;
-					System.out.println("You have already hit this position, Enter an other position.");
-				}
-				else  {
-					same = true;
-					win=player[turn].makeGuess(g);
-				}
-				
-			}
 			if(!win) turn = (turn+1)%2;
-			sc.nextLine();
-			
-		} while(!win); // while the game is not won
-	}
+				sc.nextLine();
+		}
+			while(!win); // while the game is not won
 		
-			
+			this.viewResults();		
+		
+	}
+
+
 	// View Results
+
 	private void viewResults()
 	{
-		private void viewResults()
-		{
-			char[][] firstPlayerArray = this.player[1].myGrid.displayOwnGrid();
-			char[][] secondPlayerArray = this.player[2].myGrid.displayOwnGrid();
-			char[][] firstPlayerOpponentArray = this.player[1].myGrid.displayEnnemyGrid();
-			char[][] secondPlayerOpponentArray = this.player[2].myGrid.displayEnnemyGrid();
-			int nbHitByPlayerOne = 0 ;
-			int nbHitByPlayerTwo = 0 ;
-			int nbMissByPlayerOne = 0 ;
-			int nbMissByPlayerTwo = 0 ;
-			int nbShipSunkPlayerOne = 0 ;
-			int nbShipSunkPlayerTwo = 0 ;
+		char[][] firstPlayerArray = this.player[0].myGrid.displayOwnGrid();
+		char[][] secondPlayerArray = this.player[1].myGrid.displayOwnGrid();
+		char[][] firstPlayerOpponentArray = this.player[0].myGrid.displayEnnemyGrid();
+		char[][] secondPlayerOpponentArray = this.player[1].myGrid.displayEnnemyGrid();
+		int nbHitByPlayerOne = 0 ;
+		int nbHitByPlayerTwo = 0 ;
+		int nbMissByPlayerOne = 0 ;
+		int nbMissByPlayerTwo = 0 ;
+		int nbShipSunkPlayerOne = 0 ;
+		int nbShipSunkPlayerTwo = 0 ;
 
-			//first player stats
-			for ( int i = 0 ; i < firstPlayerArray.length;i++){
-				for ( int j = 0 ; j < firstPlayerArray[i].length;j++){
-					nbShipSunkPlayerOne = 3 - this.player[1].myGrid.getShips().size();
+		//first player stats
+		for ( int i = 0 ; i < firstPlayerArray.length;i++){
+			for ( int j = 0 ; j < firstPlayerArray[i].length;j++){
+				nbShipSunkPlayerOne = 3 - this.player[0].myGrid.getShips().size();
 
-				}
 			}
-
-			for ( int i = 0 ; i < firstPlayerOpponentArray.length;i++){
-				for ( int j = 0 ; j < firstPlayerOpponentArray[i].length;j++){
-					if ( firstPlayerOpponentArray[i][j] == Grid.HIT)
-						nbHitByPlayerOne +=1;
-					else if ( firstPlayerOpponentArray[i][j] == Grid.HIT_MISSED)
-						nbMissByPlayerOne +=1;
-				}
-			}
-
-
-			//second player stats
-			for ( int i = 0 ; i < secondPlayerArray.length;i++){
-				for ( int j = 0 ; j < firstPlayerArray[i].length;j++){
-					nbShipSunkPlayerTwo = 3 - this.player[1].myGrid.getShips().size();
-
-				}
-			}
-
-			for ( int i = 0 ; i < secondPlayerOpponentArray.length;i++){
-				for ( int j = 0 ; j < secondPlayerOpponentArray[i].length;j++){
-					if ( secondPlayerOpponentArray[i][j] == Grid.HIT)
-						nbHitByPlayerTwo +=1;
-					else if ( secondPlayerOpponentArray[i][j] == Grid.HIT_MISSED)
-						nbMissByPlayerTwo +=1;
-				}
-			}
-
-			boolean winnerOrLooser = (this.player[1].myGrid.getShips().size() == 0);
-			String winner = "";
-			String looser = "";
-			if(winnerOrLooser){
-				winner = this.player[2].name;
-				looser = this.player[1].name;
-			}else{			
-				winner = this.player[1].name;
-				looser = this.player[2].name; 
-			}
-			
-
-			String res = " Results\n" ;
-			res += "\nThe previous game was won by: "+ winner+"\n"; // Should probably use the code above to display winner
-			res+= "And the looser is" + looser +"\n";
-			
-			res += "Stats Player 1\n\n"; 
-			res += " Numbers of your shot which hit the opponent : " + nbHitByPlayerOne ;
-			res += " \nNumbers of your shot which missed the opponent : " + nbMissByPlayerOne ;
-			res += " \nNumbers of your ships which sunk : " + nbShipSunkPlayerOne;
-
-
-			res += "Stats Player 1\n\n"; 
-			res += " \nNumbers of your shot which hit the opponent : " + nbHitByPlayerTwo ;
-			res += " \nNumbers of your shot which missed the opponent : " + nbMissByPlayerTwo ;
-			res += " \nNumbers of your ships which sunk : " + nbShipSunkPlayerTwo;
-			
-			System.out.println(res);
 		}
 
+		for ( int i = 0 ; i < firstPlayerOpponentArray.length;i++){
+			for ( int j = 0 ; j < firstPlayerOpponentArray[i].length;j++){
+				if ( firstPlayerOpponentArray[i][j] == Grid.HIT)
+					nbHitByPlayerOne +=1;
+				else if ( firstPlayerOpponentArray[i][j] == Grid.HIT_MISSED)
+					nbMissByPlayerOne +=1;
+			}
+		}
+
+
+		//second player stats
+		for ( int i = 0 ; i < secondPlayerArray.length;i++){
+			for ( int j = 0 ; j < firstPlayerArray[i].length;j++){
+				nbShipSunkPlayerTwo = 3 - this.player[1].myGrid.getShips().size();
+
+			}
+		}
+
+		for ( int i = 0 ; i < secondPlayerOpponentArray.length;i++){
+			for ( int j = 0 ; j < secondPlayerOpponentArray[i].length;j++){
+				if ( secondPlayerOpponentArray[i][j] == Grid.HIT)
+					nbHitByPlayerTwo +=1;
+				else if ( secondPlayerOpponentArray[i][j] == Grid.HIT_MISSED)
+					nbMissByPlayerTwo +=1;
+			}
+		}
+
+		boolean winnerOrLooser = (this.player[0].myGrid.getShips().size() == 0);
+		String winner = "";
+		String looser = "";
+		if(winnerOrLooser){
+			winner = this.player[1].name;
+			looser = this.player[0].name;
+		}else{			
+			winner = this.player[0].name;
+			looser = this.player[1].name; 
+		}
+
+
+		String res = " Results\n" ;
+		res += "\nThe previous game was won by: "+ winner+"\n"; // Should probably use the code above to display winner
+		res+= "And the looser is" + looser +"\n";
+
+		res += "Stats Player 1\n\n"; 
+		res += " Numbers of your shot which hit the opponent : " + nbHitByPlayerOne ;
+		res += " \nNumbers of your shot which missed the opponent : " + nbMissByPlayerOne ;
+		res += " \nNumbers of your ships which sunk : " + nbShipSunkPlayerOne;
+
+
+		res += "Stats Player 1\n\n"; 
+		res += " \nNumbers of your shot which hit the opponent : " + nbHitByPlayerTwo ;
+		res += " \nNumbers of your shot which missed the opponent : " + nbMissByPlayerTwo ;
+		res += " \nNumbers of your ships which sunk : " + nbShipSunkPlayerTwo;
+
+		System.out.println(res);
 	}
 
 	// Quit the Game
@@ -244,7 +215,7 @@ public class Game
 	/**
 	 * Method displaying a two dimensional array into the console.
 	 */
-	public String display(char[][] array){
+	public static String display(char[][] array){
 		int k = 4;
 		String format = "%-3s";
 		String limit = "    +"+new String(new char[array.length]).replace("\0","---+");
@@ -289,9 +260,9 @@ public class Game
 	 */
 	public static int readXAxis() {
 		Scanner sc = new Scanner(System.in);
-		String temp = sc.nextLine();
+		String temp = sc.next();
 		char c = temp.charAt(0);
-		
+
 		while ((int)c < 65 || (int)c > 74 && (int)c < 97 || (int)c > 106 || temp.length() != 1 || temp.equals("")){
 			System.out.println("This is not a valid character");
 			temp = sc.next();
@@ -300,7 +271,7 @@ public class Game
 		int x = Game.changeX(c);
 		return x;
 	}
-	
+
 	/**
 	 * Method which enables to read the Y axis.
 	 * @return the integer for the Y axis.
@@ -308,15 +279,15 @@ public class Game
 	public static int readYAxis() {
 		Scanner sc = new Scanner(System.in);
 		String temp;
-     	while (!sc.hasNextInt()) {
-     		System.out.println("invalid! You must enter an integer");
-     		sc.nextLine();
-     	}
-         temp = sc.next();
-         int y =  Integer.parseInt(temp)-1;
-         return y;
+		while (!sc.hasNextInt()) {
+			System.out.println("invalid! You must enter an integer");
+			sc.next();
+		}
+		temp = sc.next();
+		int y =  Integer.parseInt(temp)-1;
+		return y;
 	}
-	
+
 	private static int changeX(char c){ //The method changes any letter into a number
 		int i=0;
 		boolean enc=false;
@@ -328,9 +299,9 @@ public class Game
 		}
 		return i;
 	}
-	
+
 	//public void setGuess() {}
-	
+
 	public static void clearConsole() {
 		for(int i = 0; i <25;i++) {
 			System.out.println(System.getProperty("line.separator"));
