e9db55d78a18f38a7f10ea48e63b81b0ec759ff7
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-15 16:17:48 +0100

    TestGame

--- src/uk/ac/gcu/battleships/test/TestGame.java
+++ src/uk/ac/gcu/battleships/test/TestGame.java
@@ -3,12 +3,39 @@ package uk.ac.gcu.battleships.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import uk.ac.gcu.battleships.*;
 public class TestGame {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPlayerSize() {
+		Player []player = new Player[2];
+		int size = 2;
+		assertEquals("Equal", player.length, size);
+	}
+	
+	@Test
+	public void testGuess() { // This doesn't work, I know why but not sure how to fix it
+		Guess g = new Guess(0,0);
+		assertEquals("Equal", g, 0);
+	}
+	
+	@Test
+	public void testTurn() {
+		int turn = 0;
+		assertEquals("Equals", turn, 0);
+	}
+	
+	@Test
+	public void testWinFalse() {
+		boolean win = false;
+		assertEquals("Equal", win, false);
+	}
+	
+	@Test
+	public void testSame() {
+		boolean same = false;
+		assertEquals("Equal", same, false);
+		
 	}
 
 }
