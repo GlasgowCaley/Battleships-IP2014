8838f2ce132654dcdca85f598b3921b91f88ca32
Yann Prono <pronoyann@gmail.com>
2014-05-16 11:05:57 +0200

    Fix the bug (just one ship added).

--- src/uk/ac/gcu/battleships/ArtificialPlayer.java
+++ src/uk/ac/gcu/battleships/ArtificialPlayer.java
@@ -18,22 +18,23 @@ public class ArtificialPlayer extends Player{
 		hitRow=false;
 		back = false;
 		lastHit = new Guess(0,0);
-		r=(int) Math.floor(Math.random()*2);
-		l=(int) Math.floor(Math.random()*2);
-		rl=(int) Math.floor(Math.random()*4);		
+		r=(int) Math.floor(Math.random()*1);
+		l=(int) Math.floor(Math.random()*1);
+		rl=(int) Math.floor(Math.random()*3);		
 	}
 
 	public void inputName() 
 	{
 		this.name = "Computer";
 	}
+	
 
 	public boolean makeGuess(Guess G){
 		char resul;
 		do{
 			if(!hitBefore){
-				G.set_X((int)(Math.random()*5));
-				G.set_Y((int)(Math.random()*5));
+				G.set_X((int)(Math.random()*4));
+				G.set_Y((int)(Math.random()*4));
 			}
 
 			else if(hitLine)
@@ -90,9 +91,9 @@ public class ArtificialPlayer extends Player{
 				rl = (rl+1)%4;
 			}
 			//By now the computer only do randomly movements. We improve that
-			G.set_X((int)Math.random()*5);
-			G.set_Y((int)Math.random()*5);
-		}while(!(this.opponentGrid.returnCharacter(G.get_X(), G.get_Y()) == Grid.DEFAULT_CHAR));
+			//G.set_X((int)Math.random()*5);
+			//G.set_Y((int)Math.random()*5);
+		}while(!(this.opponentGrid.returnCharacter(G.get_X(), G.get_Y()) == Grid.DEFAULT_CHAR) && !(this.opponentGrid.returnCharacter(G.get_X(), G.get_Y()) == 's') && !(this.opponentGrid.returnCharacter(G.get_X(), G.get_Y()) == 'd') && !(this.opponentGrid.returnCharacter(G.get_X(), G.get_Y()) == 'b'));
 
 		resul=opponentGrid.checkGuessAI(G);
 
@@ -152,21 +153,18 @@ public class ArtificialPlayer extends Player{
 	}
 
 	public void addFleet(){ // Creates a fleet of boats
-
-		boolean add=false;
-
 		for(int i=2; i<=4; i++){
-
+			boolean add = false;
 			while(!add) {
-				add = this.addBoat(i);
+				add = this.addBoat(i);				
 			}
 		}
 	}
 
 	public boolean addBoat(int b){
 		boolean add= false;
-		int x = (int) Math.random()*4;
-		int y = (int) Math.random()*4;
+		int x = (int) (Math.random()*4);
+		int y = (int) (Math.random()*4);
 		char orient;
 		int orien = (int) Math.random();
 		if (orien==0) orient='h';
