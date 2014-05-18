3b7b332b662c567d262e1c78335ec1805a34282d
Yann Prono <pronoyann@gmail.com>
2014-05-18 16:49:26 +0200

    Remove old method for read coordinates and call the new.

--- src/uk/ac/gcu/battleships/Player.java
+++ src/uk/ac/gcu/battleships/Player.java
@@ -6,10 +6,12 @@ public class Player  {
 	public String name;
 	public Grid myGrid;
 	public Grid opponentGrid;
+	private int size;
 
 	public Player(){
 		this.inputName();
 		myGrid = new Grid();
+		this.size = this.myGrid.getBoard().length;
 	}
 
 	public void inputName() {
@@ -56,24 +58,19 @@ public class Player  {
 
 	public boolean addBoat(int b){
 		boolean add = false;
-		int l = this.myGrid.getBoard().length;
-
-		String letter = "Please enter the horizontal (A to E) coordinate of ship"; //+ count
-		String number = "Please enter the vertical (1 to 5) coordinate of ship";
+		String msg = "Please enter the couple of coordinate of ship ('A1'): "; //+ count
 		String msgOrient = "Please enter the orientation of the ship (H or V)";
-		
-		int x = Game.readXAxis(letter,l); // Get X Axis value, save to x
-		int y = Game.readYAxis(number,l); // Get Y Axis value, save to y
+
+		int coord [] = Game.readCoordinate(msg, this.size);
 		char orient = Game.readOrientation(msgOrient);
-		
-		add  = this.createShip(b,x,y,orient);
+
+		add  = this.createShip(b,coord[0],coord[1],orient);
 		return add;
 	}
 
-
 	public void enterGuess(Guess g) {
-		String dec = "      ";
 		Scanner sc = new Scanner(System.in);
+		String dec = "      ";
 		Game.clearConsole(); // Empty the screen		
 		System.out.println(dec+"-- Captain " +this.name+" --"); // Display name of player whose turn it is
 		sc.nextLine();
@@ -81,31 +78,9 @@ public class Player  {
 		System.out.print(Game.display(this.myGrid.displayOwnGrid())); // Show own grid
 		System.out.println("\n"+dec+"State of the fight"); // Display name of player whose turn it is
 		System.out.println(Game.display(this.opponentGrid.displayEnnemyGrid())); // Show enemy grid
-		System.out.println("\tYour guess\n"); // Ask for guess
-		boolean same = false;
-
-		while(!same) {
-			// 	Read the guess
-			String letter = "Enter the letter coordinate : ";
-			String number = "Enter the number coordinate : ";
-			int l = this.myGrid.getBoard().length;
-			int x = Game.readXAxis(letter,l); // Call readXAxis
-			g.set_X(x);
-
-			int y = Game.readYAxis(number,l); // Call readYAxis
-			g.set_Y(y);
-			char c = this.opponentGrid.returnCharacter(y, x);
-
-			if(c == Grid.HIT_MISSED || c == Grid.HIT) {
-				same = false;
-				System.out.println("You have already hit this position, Enter an other position.");
-			}
-			else  {
-				same = true;
-
-			}
-		}
-		
+		String msg = "Enter coordinates of you guess (like 'A1'): ";
+		int[] coord = Game.readCoordinate(msg, this.size);
+		g.set_X(coord[0]);
+		g.set_Y(coord[1]);
 	}
-
 }
