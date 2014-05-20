f59eed0b9c53b676f6603e60c736295854076db4
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-20 16:14:25 +0100

    Updated GUI stuff - work in progress
    read: it doesn't work

---
+++ src/uk/ac/gcu/battleships/view/Battleships.java
@@ -0,0 +1,19 @@
+package uk.ac.gcu.battleships.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Battleships extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	public static void main (String[] args) {
+		Application.launch(Battleships.class, args);
+	}
+
+}
--- src/uk/ac/gcu/battleships/view/GameController.java
+++ src/uk/ac/gcu/battleships/view/GameController.java
@@ -1,6 +1,12 @@
 package uk.ac.gcu.battleships.view;
 
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import uk.ac.gcu.battleships.*;
 
 /** 
@@ -9,7 +15,13 @@ import uk.ac.gcu.battleships.*;
  * @author Robert
  * @version 0.1
  */
-public class GameController {
+public class GameController extends AnchorPane {
+	
+	@FXML
+	Button btnTest;
+	
+	@FXML
+	Label playerA1;
 	
 	/**
 	 * Reference back to Game class
@@ -22,4 +34,26 @@ public class GameController {
 	public GameController () {
 		
 	}
+	
+	@FXML
+	private static void gridManager () {
+
+	}
+	
+	@FXML
+	private static void btnClick () {
+		String valueProperty = "!"; 								// Set the value to an exclamation mark
+		Button btnTest = new Button() ;								// Create a new button called btnTest
+		
+		Label playerA1 = new Label();								// Create a label called playerA1
+		
+		btnTest.setOnAction(new EventHandler<ActionEvent>() {		// When a button is clicked make a new event
+			
+			@Override
+			public void handle(ActionEvent event) {
+		        playerA1.setText("a");
+			}
+		});
+	}
 }
+
