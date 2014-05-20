5f01810fee41e8c370055cf45ae4a3f43457a369
Anthony Cerf <anthony.cerf7@etu.univ-lorraine.fr>
2014-05-20 17:38:30 +0100

    Add Id for the ship

--- src/uk/ac/gcu/battleships/GameConfiguration.java
+++ src/uk/ac/gcu/battleships/GameConfiguration.java
@@ -14,11 +14,24 @@ public class GameConfiguration {
 	public static int gameConfiguration_NBSHIP = 3 ;
 
 	/**
-	 * Static attribut which represents the type oh the ship to be placed
+	 * Static attribut which represents the id of the Destroyer
 	 */
 	
-	//public static ;
+	public static int id_Destroyer = 01 ;
+
+
+	/**
+	 * Static attribut which represents the id of the Submarine
+	 */
 	
+	public static int id_Submarine = 02 ;
+
+	/**
+	 * Static attribut which represents the id of the Battleship
+	 */
+	
+	public static int id_Battleship = 03 ;
+
 	/**
 	 * Setters of the attributs which represents the size of the Grid
 	 * @param size
