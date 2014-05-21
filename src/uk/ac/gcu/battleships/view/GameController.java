524a898cf3b46a6db74485156b64ce9c3ff6c80e
Anthony Cerf <anthony.cerf7@etu.univ-lorraine.fr>
2014-05-21 10:51:24 +0100

    Implements Observable et delete TestPlayer

--- src/uk/ac/gcu/battleships/view/GameController.java
+++ src/uk/ac/gcu/battleships/view/GameController.java
@@ -1,59 +1,50 @@
-
 package uk.ac.gcu.battleships.view;
 
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import java.util.Observable;
+
 import uk.ac.gcu.battleships.*;
 
-/** 
- * This controller method drives Game.fxml
- * Work in progress
- * @author Robert
- * @version 0.1
+/**
+ * Class which enable us to control the game
+ * @author Anthony Cerf 
+ * @author Yann Prono
  */
-public class GameController extends AnchorPane {
-	
-	@FXML
-	Button btnTest;
-	
-	@FXML
-	Label playerA1;
+
+
+public class GameController extends Observable{
+
+	/** attribut which instantiate the game*/
+	private Game game ;
+
+	/** attribut which enable to create a link with InputView */
+	private InputView iv ;
+
 	
 	/**
-	 * Reference back to Game class
+	 * Constructor which lauch the game
 	 */
-	private Game game;
-	
-	/** 
-	 * Constructor for class GameController 
-	 */
-	public GameController () {
-		
+	public GameController(){
+		game = new Game();
 	}
-	
-	@FXML
-	private static void gridManager () {
 
+	/**
+	 * method which enables to change the message
+	 * @param s = the message
+	 */
+	public void setMessage( String s ){
+		this.notifyObservers(s);
 	}
+	/**
+	 * Method which initialize the beginning of the Game
+	 */
+	public void initGame(){
+		//input the name
+		for ( int i = 0 ; i < game.getPlayer().length; i++){
+			game.getPlayer()[i] = new Player(iv.getContent());
+		}
+
+		// place the ship
 	
-	@FXML
-	private static void btnClick () {
-		String valueProperty = "!"; 								// Set the value to an exclamation mark
-		Button btnTest = new Button() ;								// Create a new button called btnTest
-		
-		Label playerA1 = new Label();								// Create a label called playerA1
-		
-		btnTest.setOnAction(new EventHandler<ActionEvent>() {		// When a button is clicked make a new event
-			
-			@Override
-			public void handle(ActionEvent event) {
-		        playerA1.setText("a");
-			}
-		});
+
 	}
 }
