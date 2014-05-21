edf9f4f7377d116eb1339b1b5adc562a18724c38
Robert Morrison <robertjmorrison1994@gmail.com>
2014-05-21 11:12:10 +0100

    Some sort of grid ready

--- src/uk/ac/gcu/battleships/view/Battleships.java
+++ src/uk/ac/gcu/battleships/view/Battleships.java
@@ -1,19 +1,45 @@
 package uk.ac.gcu.battleships.view;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class Battleships extends Application {
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+import uk.ac.gcu.battleships.*;
+
+import java.awt.BorderLayout;
+
+/**
+ * For GUI
+ * @author Robert Morrison
+ * @author Tuomas Kinnunen
+ * 
+ */
+public class Battleships {
+	public Battleships() {				
+		Grid g = new Grid();
+		GridView og = new GridView(g.displayOwnGrid());
+		GridView eg = new GridView(g.displayEnnemyGrid());
+		JFrame frame = new JFrame()	;
+		JPanel back = new JPanel(new GridLayout(1,3));
+		Instruction inst = new Instruction();
+		back.add(og);
+		back.add(inst);
+
+		back.add(eg);
+		frame.setContentPane(back);
 		
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);						\\set the window in the middle of the screen
 	}
-	
+
+	/** Main method */
 	public static void main (String[] args) {
-		Application.launch(Battleships.class, args);
+		new Battleships();
+		
 	}
-
 }
