d32d1634eaf32711a262d616f35076f8fc0c0fa2
Yann Prono <pronoyann@gmail.com>
2014-05-21 17:58:20 +0200

    Add labels for each Grid.

--- src/uk/ac/gcu/battleships/view/PlayerView.java
+++ src/uk/ac/gcu/battleships/view/PlayerView.java
@@ -26,10 +26,21 @@ public class PlayerView extends JPanel {
 	public PlayerView(Player p) {
 		super();
 		this.p = p;
+		this.setLayout(new BorderLayout());
+		JPanel main = new JPanel();		
 		this.own = new GridView(p.myGrid,p.myGrid.displayOwnGrid(),false);
-		this.ennemy = new GridView(p.myGrid,p.opponentGrid.displayEnnemyGrid(),true);		
-		this.add(ennemy);
-		this.add(own);
+		this.ennemy = new GridView(p.myGrid,p.opponentGrid.displayEnnemyGrid(),true);
+		main.add(ennemy);
+		main.add(own);
+		/*this.add(ennemy);
+		this.add(own);*/
+		this.add(main,BorderLayout.CENTER);
+		JPanel infos = new JPanel();
+		infos.setLayout(new GridLayout(1,1));
+		
+		infos.add(new JLabel("Ennemy fleet",JLabel.CENTER));
+		infos.add(new JLabel("State of your fleet",JLabel.CENTER));
+		this.add(infos,BorderLayout.SOUTH);
 		//this.add(new JLabel("Ennemy Grid",JLabel.LEFT));
 		//this.add(new JLabel("State of you fleet",JLabel.RIGHT));
 	}
