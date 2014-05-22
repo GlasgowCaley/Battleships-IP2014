9fc01ac188aefb22def2e1398b32919633fc69b5
Yann Prono <pronoyann@gmail.com>
2014-05-23 01:22:32 +0200

    Remove main method (moved in BattleshipsGUI class).

--- src/uk/ac/gcu/battleships/view/Panel.java
+++ src/uk/ac/gcu/battleships/view/Panel.java
@@ -45,19 +45,4 @@ public class Panel extends JPanel {
 		this.add(gameV,BorderLayout.CENTER);
 		
 	}
-	
-
-	public static void main ( String [] args){
-		JFrame fenetre = new JFrame();		
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenetre.setMinimumSize(new Dimension(400,300));
-		fenetre.setSize(new Dimension(700,500));
-		Game g = new Game();
-		g.initPlayer();				
-		Panel dessin = new Panel(g);
-		fenetre.setContentPane(dessin);
-		fenetre.pack();
-		fenetre.setVisible(true);
-		dessin.requestFocus();
-	}
 }
