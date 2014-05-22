bc5e8887f0cebeb3a84c1124f66cbe0825b140ee
Yann Prono <pronoyann@gmail.com>
2014-05-23 01:03:35 +0200

    Display minimal help.

--- src/uk/ac/gcu/battleships/view/GridView.java
+++ src/uk/ac/gcu/battleships/view/GridView.java
@@ -1,148 +1,82 @@
 package uk.ac.gcu.battleships.view;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.Observable;
-import java.util.Observer;
-
 import javax.swing.*;
 import javax.swing.border.Border;
-
 import uk.ac.gcu.battleships.GameConfiguration;
-import uk.ac.gcu.battleships.Grid;
-import uk.ac.gcu.battleships.Guess;
 
-
-public class GridView extends JPanel implements Observer {
+public class GridView extends JPanel {
 
 	private JLabel label[][];
-	private Grid g;
-	public static Border BLACKLINE = BorderFactory.createLineBorder(new Color(143,144,138),1);
+	private JPanel grid;
+	private JPanel infos;
 	private boolean action;
-	public Icon water = new ImageIcon("images/sea.png");
-	public Icon question = new ImageIcon("images/Help-icon.png");
-	public Icon hit = new ImageIcon("images/Military-Explosion-icon.png");
-	public Icon submarine=new ImageIcon("images/Letter-S-icon.png");
-	public Icon destructor=new ImageIcon("images/Letter-D-icon.png");
-	public Icon battleship=new ImageIcon("images/Letter-B-icon.png");
-	public Icon death=new ImageIcon("images/pirate_flag.png");
-
+	
+	public static Border BLACKLINE = BorderFactory.createLineBorder(new Color(143,144,138),1);
+	
 
 	/**
-	 * Constructor 
+	 * Constructor *
 	 * @param p Current player.
 	 */
-	public GridView(Grid g,char[][] array,boolean action) {
+	public GridView(char[][] array,boolean action) {
 		super();
-		this.setPreferredSize(new Dimension(500,300));
-		this.g = g;
+		this.grid = new JPanel();
+		this.infos = new JPanel();
+		this.setPreferredSize(new Dimension(500,500));
 		this.setBackground(new Color(39,40,34));
-		this.init(array);
+		this.grid.setBackground(new Color(39,40,34));
 		this.action = action;
-		if(action) {
+		this.init(array);		
+		if(this.action) {
 			this.initActionListener();
 		}
 	}
+	
 
 
 	private void initActionListener() {
 		int size = GameConfiguration.gameConfiguration_SIZE;
 		for (int i=0; i<size; i++) {
 			for (int j=0; j<size; j++) {
-				label[i][j].addMouseListener ( new MouseAdapter() {
-
-					public void mousePressed ( MouseEvent e )
-					{	
-						System.out.println("ok");
-						JLabel l = (JLabel)e.getSource();
-						int[] coord = search(l);
-						System.out.println(coord[0]);
-						System.out.println(coord[1]);
-						System.out.println(g.checkGuess(new Guess(coord[0],coord[1])));
-					}
-
-
-				});
+				label[i][j].addMouseListener ( new GameController());
+				
 			}
 		}
 
 	}
-
-	public int[] search(JLabel l) {
-		int x = 0;
-		int y = 0;
-		int size = GameConfiguration.gameConfiguration_SIZE;
-		boolean found = false;
-		int i = 0,j = 0;
-		while(!found && i<size) {
-			j = 0;
-			while(!found && j<size) {
-				if(this.label[i][j].equals(l)) 
-					found = true;				
-				j++;
-			}			
-			i++;
-		}
-
-		if(found) {
-			x = j-1;
-			y = i-1;
-		}
-		return new  int[]{x,y};
-
-	}
-
 
 	private void init(char[][] array) {
 		int size = GameConfiguration.gameConfiguration_SIZE;
 		this.label = new JLabel[size][size];
-		GridLayout layout = new GridLayout(size,size);
+		this.grid.setLayout(new GridLayout(size,size));
+		this.infos.setLayout(new GridLayout(1,1));
+		BorderLayout layout = new BorderLayout();
 		this.setLayout(layout);
 		for (int i=0; i<size; i++) {
 			for (int j=0; j<size; j++) {
-				char op = array[i][j];
-				label[i][j] = new JLabel(Character.toString(array[i][j]));
-				/*switch(op){
-					case '-':
-						label[i][j].setIcon(question);
-						break;
-					case 'x':
-						label[i][j].setIcon(hit);
-						break;
-					case 'o':
-						label[i][j].setIcon(water);
-						break;
-					case 'D':
-						label[i][j].setIcon(destructor);
-						break;
-					case 'B':
-						label[i][j].setIcon(battleship);
-						break;
-					case 'S':
-						label[i][j].setIcon(submarine);
-						break;
-					case '@':
-						label[i][j].setIcon(death);
-						break;
-					default:
-						label[i][j].setIcon(water);
-						break;
-					
-				}*/
-				label[i][j].setBorder(GridView.BLACKLINE);
-				//this.add(label[i][j]);
-				
 				label[i][j] = new JLabel(Character.toString(array[i][j]),JLabel.CENTER);
-				label[i][j].setFont(new Font("Arial",Font.BOLD,15));
-				label[i][j].setForeground(Color.WHITE);
 				label[i][j].setBorder(GridView.BLACKLINE);
-				this.add(label[i][j]);
+				label[i][j].setFont(new Font("Arial",Font.BOLD,15));
+				label[i][j].setForeground(Color.WHITE);					
+				label[i][j].setOpaque(false);
+				label[i][j].setName(String.valueOf(j)+String.valueOf(i));
+				label[i][j].setBorder(GridView.BLACKLINE);
+				this.grid.add((label[i][j]));
 			}
 		}
+		this.add(this.grid,BorderLayout.CENTER);
+		System.out.println("method");
+		System.out.println(this.action);
+		if(this.action)
+			
+			this.infos.add(new JLabel("Click on the left grid to make a guess",JLabel.CENTER));
+		else 
+			this.infos.add(new JLabel("State of your fleet",JLabel.CENTER));
+		this.add(this.infos,BorderLayout.SOUTH);
+	}
+	
+	public JLabel[][] getGrid() {
+		return this.label;
 	}
 
-	@Override
-	public void update(Observable o, Object arg) {
-		
-	}
 }
